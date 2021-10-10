9d7e4c2c939f02e8b2b1b8e4ea4caf7894b9944c
Kartik <itskwadehra@gmail.com>
2021-10-10 16:03:10 +0530

    Adding code coverage

--- src/main/java/com/bits/devops/assignment/cicdtest/CicdtestApplication.java
+++ src/main/java/com/bits/devops/assignment/cicdtest/CicdtestApplication.java
@@ -3,11 +3,15 @@ package com.bits.devops.assignment.cicdtest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class CicdtestApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CicdtestApplication.class, args);
+
 	}
 
 }
